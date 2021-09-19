4a459d9b4f2bfb6a3f4abdfe6fbed9769e64cb0a
Kayang-Arkar <90438180+Kayang-Arkar@users.noreply.github.com>
2021-09-19 20:25:41 +0630

    Add files via upload

---
+++ hello1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Donate Via ( No Money, No Worries, Just click )
+# ## This site allows you to donate for revolution without the need for a single penny. Just Click, Stream and Have Fun !!!
+
+# ## >>YouTube
+# ### Description:
+# #### - Stream with VPN ( US region, Europe region,etc,. )
+# #### - Watch till the end without pausing
+# #### - Watch ads without skipping..
+
+# [![my image](http://img.youtube.com/vi/jBfGpgNchRA/0.jpg)](https://www.youtube.com/watch?v=jBfGpgNchRA)
+# 
+
+# [![my imgae](https://img.youtube.com/vi/W6SJ0bJLWB0/0.jpg)](https://www.youtube.com/watch?v=W6SJ0bJLWB0)
+
+# ## >>Spring Zodiac
+
+# * [Go to Link 1](https://ouo.io/3HxUti)
+# * [Go to Link 2](https://ouo.io/Wpjf9s)
+# * [Go to Link 3](https://ouo.io/mvn3vjF)
+# 
+
+# ## >>Click to donate
+
+# * [Go to Link 1](https://ouo.io/TY9AZB)
+# * [Go to Link 2](https://ouo.io/OUlxH2)
+# * [Go to Link 3](https://ouo.io/sWNu8EA)
+
+# ### Thank you for visiting our site...
+# ### Come and visit our site tomorrow...
+
+# >WE MUST WIN. III
